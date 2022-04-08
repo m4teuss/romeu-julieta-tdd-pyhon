a7cae280371a58d78cb5bcbc639b5d99d7a02c99
m4teuss <mateuus.s.silva@gmail.com>
2022-04-08 10:46:48 -0300

    Refactoring code

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
 from operator import mod, eq
+from typing import Callable, Any
+from functools import wraps
 
+# comopose recebe n funcoes
+def compose(*funcs):
+    # funcao interna que recebe argumento x
+    def inner(arg):
+        # estado recebe o argumento
+        state = arg
+        for func in reversed(funcs):
+            # receba o estado da funcao anterior
+            state = func(state)
+        return state
+    return inner
+
+# decorador validar int
+def is_int(func: Callable) -> Callable:
+    @wraps(func)
+    def inner(val: Any) -> Any:
+        return func(val) if isinstance(val, int) else val
+
+    return inner
+
+@is_int
 # função recebe numero inteiro e retorna uma string
 def queijo(numero: int) -> str:
     return 'queijo' if eq(mod(numero, 3), 0) else numero
 
+
+@is_int
 def queijo_goiabada(numero: int) -> str:
     return 'romeu e julieta' if eq(mod(numero, 3), 0) and eq(mod(numero, 5), 0 ) else numero
 
+
+@is_int
 def goiabada(numero: int) -> str:
     return 'goiabada' if eq(mod(numero, 5), 0) else numero
 
-def romeu_julieta(valorEntrada:int):
-    if queijo_goiabada(valorEntrada) == 'romeu e julieta':
-        return 'romeu e julieta'
+# def romeu_julieta(valorEntrada:int):
+#     if queijo_goiabada(valorEntrada) == 'romeu e julieta':
+#         return 'romeu e julieta'
 
-    if queijo(valorEntrada) == 'queijo':
-        return 'queijo'
+#     if queijo(valorEntrada) == 'queijo':
+#         return 'queijo'
     
-    if goiabada (valorEntrada) == 'goiabada':
-        return 'goiabada'
+#     if goiabada (valorEntrada) == 'goiabada':
+#         return 'goiabada'
 
+romeu_julieta = compose(goiabada, queijo, queijo_goiabada)
